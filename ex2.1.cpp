f48e059d7922c2b41626cb7bb246330879a690d4
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-06-23 21:58:30 -0400

    Create ex2.1.cpp

---
+++ ex2.1.cpp
@@ -0,0 +1,17 @@
+/*creating linked list*/
+
+class LinkedList{
+  LinkedList* next;
+  int val;
+  
+  LinkedList(): val(0), next(nullptr)
+  LinkedList(int val): next(nullptr), val(val) {}
+  LinkedList(int val, LinkedList *next): val(val), next(node) {}
+  
+  
+  
+
+};
+
+
+
