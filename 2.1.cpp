484679bbf637677ce3aeea79a6616a467e74ed50
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-06-24 11:08:19 -0400

    Create 2.1.cpp

---
+++ 2.1.cpp
@@ -0,0 +1,35 @@
+/*
+understand
+
+8->6->8->7->2->1
+8 6 7 2 1
+
+1->2->1
+1 2
+
+3->3->1
+3 1
+
+1->2
+1 2
+
+1
+1
+
+match
+two pointer
+
+
+multi pass
+go through once store in an array
+sort the array 
+remove duplicates
+create ll
+
+sort the list
+same elements together
+if previous matching next delete one 
+
+
+
+*/
